28df1923ce56320b83aae86bb5d5f85a5c51edcc
balexey88 <balexey88@gmail.com>
2024-03-27 15:32:28 +0200

    Fix Plugin Check issues, Prepare for publishing

--- class-wpforms-lite.php
+++ class-wpforms-lite.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Compatibility Plugin Name: Contact Form by WPForms – Drag & Drop Form Builder for WordPress
- * Compatibility Plugin URI: https://wordpress.org/plugins/wpforms-lite/
- *
- * Compatibility Description: Disables filename randomization for WPForms pages.
- *
- */
-
-namespace WPSL\WPForms;
+namespace SLCA\WPForms;
 
 use wpCloud\StatelessMedia\Compatibility;
 
+/**
+ * Class WPForms
+ */
 class WPForms extends Compatibility {
   protected $id = 'wpforms';
   protected $title = 'WPForms';
@@ -18,13 +13,21 @@ class WPForms extends Compatibility {
   protected $description = 'Ensures compatibility with WPForms.';
   protected $plugin_file = ['wpforms-lite/wpforms.php', 'wpforms/wpforms.php'];
 
-  /**
+  /** 
    * @param $sm
    */
   public function module_init($sm) {
-    // exclude randomize_filename from wpforms page
-    if (!empty($_GET['page']) && $_GET['page'] == 'wpforms-builder') {
-        remove_filter('sanitize_file_name', array("wpCloud\StatelessMedia\Utility", 'randomize_filename'), 10);
+    add_action('current_screen', [$this, 'disable_cache_busting'], 20);
+  }
+
+  /**
+   * Disable cache busting for WPForms Builder page
+   * 
+   * @param $screen
+   */
+  public function disable_cache_busting($screen) {
+    if ( $screen->id === 'wpforms_page_wpforms-builder' ) {
+      remove_filter('sanitize_file_name', array("wpCloud\StatelessMedia\Utility", 'randomize_filename'), 10);
     }
   }
 }
