5f087d1c106fefbebb629532399936b9e1f6ab14
balexey88 <balexey88@gmail.com>
2024-07-02 23:29:40 +0300

    Prevent cache-busting for WPFroms uploaded files

--- class-wpforms-lite.php
+++ class-wpforms-lite.php
@@ -17,7 +17,8 @@ class WPForms extends Compatibility {
    * @param $sm
    */
   public function module_init($sm) {
-    add_action('current_screen', [$this, 'disable_cache_busting'], 20);
+    add_action( 'current_screen', [$this, 'disable_cache_busting'], 20);
+    add_filter( 'stateless_skip_cache_busting', array( $this, 'skip_cache_busting' ), 10, 2 );
   }
 
   /**
@@ -29,5 +30,24 @@ class WPForms extends Compatibility {
     if ( $screen->id === 'wpforms_page_wpforms-builder' ) {
       remove_filter('sanitize_file_name', array("wpCloud\StatelessMedia\Utility", 'randomize_filename'), 10);
     }
+  }
+
+  /**
+   * WPForms uses their own cache-busting approach so we need to skip ours
+   * 
+   * @param $null
+   * @param $filename
+   * @return bool | string
+   */
+  public function skip_cache_busting( $null, $filename ) {
+    $backtrace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS );
+
+    foreach( $backtrace as $trace ) {
+      if ( isset( $trace['class'] ) && in_array( $trace['class'], ['WPForms_Field_File_Upload', 'WPForms_Process'] ) ) {
+        return $filename;
+      }
+    }
+
+    return $null;
   }
 }
